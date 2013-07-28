75a969491696b8be5fe30027d1a17f828716fc89
CPerez <cperez1087@gmail.com>
2013-07-28 14:07:17 -0700

    Initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestBuyScrape::Application.config.secret_token = 'ecbfc0736fa4c200623cbd79836ab475031f4cc6d035401c693256f8cdfbeb6b3cfe394b28d973e1576c8a38a9b057120ac4bd2cd4b88519b655b770720d3649'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BestBuyScrape::Application.config.session_store :cookie_store, key: '_BestBuyScrape_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BestBuyScrape::Application.config.session_store :active_record_store
